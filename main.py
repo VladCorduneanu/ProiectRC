b5cedc59257184bb6798f079be0875b5b8070a84
AlexandruuBaciu <AlexandruuBaciu@users.noreply.github.com>
2019-10-09 23:09:45 +0300

    Solution for: Add a controller for the state of the application(Receiver or Transmitter)

--- main.py
+++ main.py
@@ -5,39 +5,12 @@ def main():
     # Interface Controller
     interface_controller = InterfaceController.get_instance()
 
-    # Master
-    interface_controller.master = Tk()
-    interface_controller.master.geometry("600x600")
+    # State Controller
+    state_controller = StateController.get_instance()
 
-    # Frame
-    interface_controller.frame = Frame(interface_controller.master, background="lightblue")
-    interface_controller.frame.pack(fill="both", expand=True)
+    # Master Loop
+    interface_controller.get_instance().master.mainloop()
 
-    # Change State Button
-    interface_controller.change_state_button = Button(interface_controller.frame, text="Change State",
-                                                      command=interface_controller.change_state)
-    interface_controller.change_state_button.pack(side=BOTTOM, padx=5, pady=5)
-
-    # State Label Text
-    interface_controller.state_label_text = StringVar(interface_controller.master)
-    interface_controller.state_label_text.set("State: Transfer")
-
-    # State Label
-    interface_controller.state_label = Label(interface_controller.frame,
-                                             textvariable=interface_controller.state_label_text, font=20)
-    interface_controller.state_label.pack(side=BOTTOM)
-
-    # Transfer Button
-    interface_controller.transfer_button = Button(interface_controller.frame, text="Transfer File",
-                                                  command=interface_controller.transfer_file)
-    interface_controller.transfer_button.pack(padx=5, pady=5)
-
-    # Text Box for file path
-    interface_controller.text_box = Entry(interface_controller.frame, width=200)
-    interface_controller.text_box.pack(padx=5, pady=5)
-
-    # MainLoop
-    interface_controller.master.mainloop()
 
 
 class InterfaceController:
@@ -54,7 +27,47 @@ class InterfaceController:
         # Variables
         self.state_label_text = None
         self.state = "Transfer"
+
+        # Init Variables
+        self.InitInterfaceController()
+        # TODO INIT
         pass
+
+    # Init function
+
+    def InitInterfaceController(self):
+        # Master
+        self.master = Tk()
+        self.master.geometry("600x600")
+
+        # Frame
+        self.frame = Frame(self.master, background="lightblue")
+        self.frame.pack(fill="both", expand=True)
+
+        # Change State Button
+        self.change_state_button = Button(self.frame, text="Change State",
+                                          command=self.change_state)
+        self.change_state_button.pack(side=BOTTOM, padx=5, pady=5)
+
+        # State Label Text
+        self.state_label_text = StringVar(self.master)
+        self.state_label_text.set("State: Transfer")
+
+        # State Label
+        self.state_label = Label(self.frame,
+                                 textvariable=self.state_label_text, font=20)
+        self.state_label.pack(side=BOTTOM)
+
+        # Transfer Button
+        self.transfer_button = Button(self.frame, text="Transfer File",
+                                      command=self.transfer_file)
+        self.transfer_button.pack(padx=5, pady=5)
+
+        # Text Box for file path
+        self.text_box = Entry(self.frame, width=200)
+        self.text_box.pack(padx=5, pady=5)
+
+    pass
 
     # Static instance
     instance = None
@@ -68,28 +81,66 @@ class InterfaceController:
 
     @staticmethod
     def transfer_file():
-        print("clicked Transfer File")
-        input_text = InterfaceController.get_instance().text_box.get()
-        open(input_text, "+w")
+        StateController.get_instance().transfer_function()
+
     pass
 
     # TODO call State Controller to change the state and give the label what to write
     @staticmethod
     def change_state():
-        print("clicked Change State!")
-        InterfaceController.get_instance().change_state_text()
+        StateController.get_instance().change_state_text()
+
     pass
+
+    def SetStateLabelText(self, new_text):
+        self.state_label_text.set("State: " + new_text)
+
+    pass
+
+
+class StateController:
+
+    # Variables
+    def __init__(self):
+        self.state = "Transfer"
+
+    pass
+
+    # Static instance
+
+    instance = None
 
     # Class Methods
 
     def change_state_text(self):
+        print("clicked Change State!")
         if self.state == "Transfer":
             self.state = "Receive"
         else:
             self.state = "Transfer"
 
-        self.state_label_text.set("State: " + self.state)
+        InterfaceController.get_instance().SetStateLabelText(self.state)
 
+    pass
+
+    def transfer_function(self):
+        print("clicked Transfer File")
+        input_text = InterfaceController.get_instance().text_box.get()
+        try:
+            open(input_text, "+w")
+        except IOError:
+            print("Error: File does not appear to exist.")
+
+    pass
+
+    @staticmethod
+    def get_instance():
+        if StateController.instance is None:
+            StateController.instance = StateController()
+        return StateController.instance
+
+    pass
 
 if __name__ == "__main__":
     main()
+
